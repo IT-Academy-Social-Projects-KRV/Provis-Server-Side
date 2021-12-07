1bf872b9c40a59a9d3adfd4c549c8d1be7d02332
AndriyKovalets <andriykovalec@gmail.com>
2021-12-07 23:19:14 +0200

    Add provis dbContext.

---
+++ Provis.Infrastructure/Data/ProvisDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Provis.Infrastructure.Data
+{
+    public class ProvisDbContext: IdentityDbContext
+    {
+        public ProvisDbContext(DbContextOptions<ProvisDbContext> options): base(options)
+        {
+
+        }
+    }
+}
