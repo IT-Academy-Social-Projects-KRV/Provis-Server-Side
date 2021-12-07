bd802f819e13482d690f044bd9e06b3a2e762584
AndriyKovalets <andriykovalec@gmail.com>
2021-12-07 23:17:11 +0200

    Add base repository interface.

---
+++ Provis.Core/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Provis.Core.Interfaces.Repositories
+{
+    public interface IRepository<TEntity> where TEntity: class, IBaseEntity
+    {
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+        IQueryable<TEntity> Query(params Expression<Func<TEntity, object>>[] includes);
+        Task<int> SaveChangesAsync();
+    }
+}
