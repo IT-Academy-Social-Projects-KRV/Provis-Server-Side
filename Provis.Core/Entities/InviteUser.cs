fd06e34d0628eee9dc3752e92a6d14e0cf29bcbf
mayor.sheka <mayor.sheka@ukr.net>
2021-12-09 10:36:32 +0300

    create inviteUser Entity

---
+++ Provis.Core/Entities/InviteUser.cs
@@ -0,0 +1,26 @@
+﻿using Provis.Core.Interfaces;
+using System;
+
+namespace Provis.Core.Entities
+{
+    public class InviteUser : IBaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool IsConfirm { get; set; }
+
+        public int WorkspaceId { get; set; }
+
+        public Workspace Workspace { get; set; }
+
+        public string FromUserId { get; set; }
+
+        public string ToUserId { get; set; }
+
+        public User FromUser { get; set; }
+
+        public User ToUser { get; set; }
+    }
+}
