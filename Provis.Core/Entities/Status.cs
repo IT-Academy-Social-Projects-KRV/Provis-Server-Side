567f64069f7dba8f030066a66b6c1c55552683ff
mayor.sheka <mayor.sheka@ukr.net>
2021-12-09 10:28:54 +0300

    create status entity

---
+++ Provis.Core/Entities/Status.cs
@@ -0,0 +1,11 @@
+﻿using Provis.Core.Interfaces;
+
+namespace Provis.Core.Entities
+{
+    public class Status : IBaseEntity
+    {
+        public int Id { get; set; }
+
+        public string StatusName { get; set; }
+    }
+}
