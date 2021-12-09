c475bcddb77d82eaa89a53d200307b440eb07f49
mayor.sheka <mayor.sheka@ukr.net>
2021-12-09 10:22:26 +0300

    Create comment Entity

---
+++ Provis.Core/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using Provis.Core.Interfaces;
+using System;
+
+namespace Provis.Core.Entities
+{
+    public class Comment : IBaseEntity
+    {
+        public int Id { get; set; }
+
+        public string CommentText { get; set; }
+
+        public int TaskId { get; set; }
+
+        public string UserId { get; set; }
+
+        public DateTime DateOfCreate { get; set; }
+
+        public Task Task { get; set; }
+
+        public User User { get; set; }
+    }
+}
