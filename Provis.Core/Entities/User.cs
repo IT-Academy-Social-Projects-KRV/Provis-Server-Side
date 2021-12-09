d1d4099c2f2cd891ed4c3a457794a48dd95642f5
mayor.sheka <mayor.sheka@ukr.net>
2021-12-09 10:40:01 +0300

    update user

--- Provis.Core/Entities/User.cs
+++ Provis.Core/Entities/User.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Provis.Core.Interfaces;
+using System;
 
 namespace Provis.Core.Entities
 {
     public class User : IdentityUser, IBaseEntity
     {
         public string Surname { get; set; }
+
+        public DateTime CreateDate { get; set; }
     }
 }
