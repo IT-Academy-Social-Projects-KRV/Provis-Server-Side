66573d973481889106d68260ab554af2a80ca568
mayor.sheka <mayor.sheka@ukr.net>
2021-12-09 10:31:14 +0300

    create statusHistory entity

---
+++ Provis.Core/Entities/StatusHistory.cs
@@ -0,0 +1,20 @@
+﻿using Provis.Core.Interfaces;
+using System;
+
+namespace Provis.Core.Entities
+{
+    public class StatusHistory : IBaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime DateOfChange { get; set; }
+
+        public int TaskId { get; set; }
+
+        public Task Task { get; set; }
+
+        public int StatusId { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
