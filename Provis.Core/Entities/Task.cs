a538b324338b47e420a01e02ca4b3c56de53c3e4
mayor.sheka <mayor.sheka@ukr.net>
2021-12-09 10:26:39 +0300

    create workspace entity

---
+++ Provis.Core/Entities/Task.cs
@@ -0,0 +1,30 @@
+﻿using Provis.Core.Interfaces;
+using System;
+
+namespace Provis.Core.Entities
+{
+    public class Task : IBaseEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateOfCreate { get; set; }
+
+        public DateTime DateOfEnd { get; set; }
+
+        public int StatusId { get; set; }
+
+        public Status Status { get; set; }
+
+        public int WorkspaceId { get; set; }
+
+        public Workspace Workspace { get; set; }
+
+        public string TaskCreaterId { get; set; }
+
+        public User User { get; set; }
+    }
+}
