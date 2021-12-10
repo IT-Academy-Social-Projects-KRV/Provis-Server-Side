34f6bd00f90e1b473785e650d63f44c2f78208d3
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-12-10 12:35:39 +0200

    add rules for UserLogValidation

---
+++ Provis.Core/Validation/UserLogValidation.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Provis.Core.DTO.userDTO;
+using Provis.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provis.Core.Validation
+{
+    class UserLogValidation : AbstractValidator<UserLogDTO>
+    {
+        public UserLogValidation()
+        {
+            RuleFor(user => user.Email)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(user => user.Password)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
