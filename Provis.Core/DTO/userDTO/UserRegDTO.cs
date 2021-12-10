c934a0ecb43d4d42ceff6ae431e85ef6f552fbbd
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-12-10 08:42:29 +0200

    add rules for UserRegDTO

--- Provis.Core/DTO/userDTO/UserRegDTO.cs
+++ Provis.Core/DTO/userDTO/UserRegDTO.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Provis.Core.DTO.userDTO
 {
-    class UserRegDTO
+    public class UserRegDTO
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
--- Provis.Core/Validation/UserRegistrationValidation.cs
+++ Provis.Core/Validation/UserRegistrationValidation.cs
@@ -1,12 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using FluentValidation;
+using Provis.Core.DTO.userDTO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.Threading;
 
 namespace Provis.Core.Validation
 {
-    class UserRegistrationValidation
+    class UserRegistrationValidation : AbstractValidator<UserRegDTO>
     {
+        private static UserManager<IdentityUser> _userManager;
+
+        public UserRegistrationValidation(UserManager<IdentityUser> manager)
+        {
+            _userManager = manager;
+
+            RuleFor(user => user.Name)
+                .NotNull()
+                .Length(3, 50);
+
+            RuleFor(user => user.Surname)
+                .NotNull()
+                .Length(3, 50);
+
+            RuleFor(user => user.Username)
+                .NotNull()
+                .Length(3, 50)
+                .MustAsync(IsUniqueUserName).WithMessage("{PropertyName} already exists.");
+
+
+            RuleFor(user => user.Email)
+                .NotNull()
+                .EmailAddress()
+                .MustAsync(IsUniqueUserEmail).WithMessage("{PropertyName} already exists.");
+
+            RuleFor(user => user.Password)
+                .NotEmpty()
+                .MinimumLength(8)
+                .Matches("[A-Z]").WithMessage("'{PropertyName}' must contain one or more capital letters.")
+                .Matches("[a-z]").WithMessage("'{PropertyName}' must contain one or more lowercase letters.")
+                .Matches(@"\d").WithMessage("'{PropertyName}' must contain one or more digits.")
+                .Matches(@"[][""!@$%^&*(){}:;<>,.?/+_=|'~\\-]").WithMessage("'{ PropertyName}' must contain one or more special characters.")
+                .Matches("^[^£# “”]*$").WithMessage("'{PropertyName}' must not contain the following characters £ # “” or spaces.");
+        }
+
+        private async Task<bool> IsUniqueUserName(string username, CancellationToken cancellationToken)
+        {
+            var userObject = await _userManager.FindByNameAsync(username);
+            if (userObject != null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private async Task<bool> IsUniqueUserEmail(string email, CancellationToken cancellationToken)
+        {
+            var userObject = await _userManager.FindByEmailAsync(email);
+            if (userObject != null)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
