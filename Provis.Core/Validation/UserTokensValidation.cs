46e6152148da95de9c3b7e17ce5b604ac3a96081
AndriyKovalets <andriykovalec@gmail.com>
2021-12-14 00:15:58 +0200

    Add valitation for tokens dto.

---
+++ Provis.Core/Validation/UserTokensValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Provis.Core.DTO.userDTO;
+
+namespace Provis.Core.Validation
+{
+    class UserTokensValidation: AbstractValidator<UserTokensDTO>
+    {
+        public UserTokensValidation()
+        {
+            RuleFor(user => user.Token)
+               .NotEmpty()
+               .NotNull();
+
+            RuleFor(user => user.RefreshToken)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
