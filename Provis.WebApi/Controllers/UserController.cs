81ba6c5ff424629b6e78d25f4b1cb0297838f0ef
Artem672 <synytsiaartem07@gmail.com>
2021-12-17 09:04:25 +0200

    Add functional for getting profile info

---
+++ Provis.WebApi/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Provis.Core.Interfaces.Services;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Provis.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private string UserId => User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("getpersonalinfo")]
+        public async Task<IActionResult> GetUserPersonalInfoAsync()
+        {
+            var userInfo = await _userService.GetUserPersonalInfoAsync(UserId);
+
+            return Ok(userInfo);
+        }
+
+    }
+}
