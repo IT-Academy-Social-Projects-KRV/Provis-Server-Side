75720994b0b0371470aace74568c1a0dcc09dbd1
mayor.sheka <mayor.sheka@ukr.net>
2021-12-20 02:20:57 +0300

    update service configuration

--- Provis.Core/StartupSetup.cs
+++ Provis.Core/StartupSetup.cs
@@ -3,6 +3,7 @@ using FluentValidation.AspNetCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Provis.Core.Helpers;
+using Provis.Core.Helpers.Mails;
 using Provis.Core.Interfaces.Services;
 using Provis.Core.Services;
 using Provis.Core.Validation;
@@ -16,7 +17,7 @@ namespace Provis.Core
             services.AddScoped<IAuthenticationService, AuthenticationService>();
             services.AddScoped<IWorkspaceService, WorkspaceService>();
             services.AddScoped<IJwtService, JwtService>();
-            services.AddScoped<IEmailSenderService, EmailSenderService>();
+            services.AddTransient<IEmailSenderService, EmailSenderService>();
             services.AddScoped<ISmtpService, SmtpService>();
             services.AddScoped<IUserService,UserService>();
         }
@@ -29,6 +30,11 @@ namespace Provis.Core
         public static void ConfigJwtOptions(this IServiceCollection services, IConfiguration config)
         {
             services.Configure<JwtOptions>(config);
+        }
+
+        public static void ConfigureMailSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<MailSettings>(configuration.GetSection("EmailSettings"));
         }
 
         public static void AddAutoMapper(this IServiceCollection services)
