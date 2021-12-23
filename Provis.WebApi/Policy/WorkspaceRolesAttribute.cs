56bc9a73644921360161d7e71121c3a7da11e62f
AndriyKovalets <andriykovalec@gmail.com>
2021-12-23 13:21:03 +0200

    Add attribute.

---
+++ Provis.WebApi/Policy/WorkspaceRolesAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+
+namespace Provis.WebApi.Policy
+{
+    public class WorkspaceRolesAttribute : AuthorizeAttribute
+    {
+
+        const string POLICY_PREFIX = "WorkspaceRoles";
+
+        public WorkspaceRolesAttribute(int[] workspaceRolesId) => WorkspaceRolesId = workspaceRolesId;
+
+        public int[] WorkspaceRolesId
+        {
+            get
+            {
+                var workspaceRoles = Policy.Substring(POLICY_PREFIX.Length + 1).Split(":");
+
+                var workspaceRolesId = Array.ConvertAll(workspaceRoles, int.Parse);
+                return workspaceRolesId;
+            }
+            set
+            {
+                Policy = $"{POLICY_PREFIX}:{String.Join(":", value)}";
+            }
+        }
+    }
+}
