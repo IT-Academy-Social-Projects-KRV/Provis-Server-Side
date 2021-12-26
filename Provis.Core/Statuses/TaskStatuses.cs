bde920b3f86f03e95a65fd20ae9f4065606a0c41
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-12-26 16:04:15 +0200

    Add statuses folder and add TaskStatuses

---
+++ Provis.Core/Statuses/TaskStatuses.cs
@@ -0,0 +1,12 @@
+﻿namespace Provis.Core.Statuses
+{
+    public enum TaskStatuses : int
+    {
+        ToDoId = 1,
+        InProgressId = 2,
+        InReviewId = 3,
+        OnHoldId = 4,
+        CompleatedId = 5,
+        ClosedId = 6
+    }
+}
