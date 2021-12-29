76874b488730e8efaf888138aeaf6d29f3a0e326
AndriyKovalets <andriykovalec@gmail.com>
2021-12-30 01:17:48 +0200

    Add file service.

---
+++ Provis.Core/Interfaces/Services/IFileService.cs
@@ -0,0 +1,13 @@
+﻿using Provis.Core.ApiModels;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Provis.Core.Interfaces.Services
+{
+    public interface IFileService
+    {
+        Task<string> AddFileAsync(Stream stream, string folderName, string fileName);
+        Task<DownloadFile> GetFileAsync(string path);
+        Task DeleteFileAsync(string path);
+    }
+}
