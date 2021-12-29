8d828672a04fd55051a9cf0b3b7efd02d03974e8
AndriyKovalets <andriykovalec@gmail.com>
2021-12-30 01:14:05 +0200

    Add file exceptions.

---
+++ Provis.Core/Exeptions/FileExceptions/FileException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Provis.Core.Exeptions.FileExceptions
+{
+    public class FileException: Exception
+    {
+        public override string Message { get; }
+        public string Path { get; }
+        public FileException(string message, string path)
+        {
+            Message = message;
+            Path = path;
+        }
+    }
+}
---
+++ Provis.Core/Exeptions/FileExceptions/FileFolderNotExistException.cs
@@ -0,0 +1,10 @@
+﻿namespace Provis.Core.Exeptions.FileExceptions
+{
+    public class FileFolderNotExistException: FileException
+    {
+        public FileFolderNotExistException(string path) : base("Cannot save the file", path)
+        {
+
+        }
+    }
+}
