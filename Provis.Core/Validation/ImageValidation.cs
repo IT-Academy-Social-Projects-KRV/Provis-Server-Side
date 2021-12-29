06ed5f830b304cb6b1c6a381b9994f2addfbe3e0
AndriyKovalets <andriykovalec@gmail.com>
2021-12-30 01:22:26 +0200

    Add validation for image.

---
+++ Provis.Core/Validation/ImageValidation.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Options;
+using Provis.Core.DTO.userDTO;
+using Provis.Core.Helpers;
+using System;
+using System.Linq;
+
+namespace Provis.Core.Validation
+{
+    public class ImageValidation: AbstractValidator<UploadImageDTO>
+    {
+        private readonly IOptions<ImageSettings> options;
+
+        public ImageValidation(IOptions<ImageSettings> options)
+        {
+            this.options = options;
+
+            RuleFor(x => x.Image)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(x => x.Image.Length)
+                .LessThanOrEqualTo(options.Value.MaxSize * 1024 * 1204)
+                .WithMessage($"Max size is {options.Value.MaxSize} Mb");
+
+            RuleFor(x => x.Image.ContentType)
+               .Must(IsImage)
+                .WithMessage("File must be image");
+        }
+
+        private bool IsImage(string contentType)
+        {
+            var type = contentType.Split("/");
+
+            if(type[0] != options.Value.Type)
+            {
+                return false;
+            }
+
+            return options.Value.Subtypes.Contains(type[1]);
+        }
+    }
+}
