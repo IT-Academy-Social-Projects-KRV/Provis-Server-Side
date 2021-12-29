8deee5efcaedb654caa4cb0af827377a0a2b70e9
AndriyKovalets <andriykovalec@gmail.com>
2021-12-30 01:14:05 +0200

    Add file exceptions.

---
+++ Provis.Core/Exeptions/FileExceptions/FileNameAlreadyExistException.cs
@@ -0,0 +1,9 @@
+﻿namespace Provis.Core.Exeptions.FileExceptions{
+    public class FileNameAlreadyExistException: FileException
+    {
+        public FileNameAlreadyExistException(string path) : base("File with this name already exist", path)
+        {
+
+        }
+    }
+}
