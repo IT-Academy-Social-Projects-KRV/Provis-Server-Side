6a8b69e3267ee1a5ee9f423fdb0190b619fa0cd0
AndriyKovalets <andriykovalec@gmail.com>
2021-12-30 19:18:04 +0200

    Resolve conflicts.

--- Provis.Core/StartupSetup.cs
+++ Provis.Core/StartupSetup.cs
@@ -19,9 +19,14 @@ namespace Provis.Core
             services.AddScoped<IJwtService, JwtService>();
             services.AddTransient<IEmailSenderService, EmailSenderService>();
             services.AddScoped<ISmtpService, SmtpService>();
+<<<<<<< HEAD
             services.AddScoped<ITaskService, TaskService>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IConfirmEmailService, ConfirmEmailService>();
+=======
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IFileService, FileService>();
+>>>>>>> efcbdbe... Add configure for image and file settings.
         }
 
         public static void AddFluentValitation(this IServiceCollection services)
@@ -37,6 +42,16 @@ namespace Provis.Core
         public static void ConfigureMailSettings(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<MailSettings>(configuration.GetSection("EmailSettings"));
+        }
+
+        public static void ConfigureImageSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<ImageSettings>(configuration.GetSection("ImageSettings"));
+        }
+
+        public static void ConfigureFileSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<FileSettings>(configuration.GetSection("FileSettings"));
         }
 
         public static void AddAutoMapper(this IServiceCollection services)
