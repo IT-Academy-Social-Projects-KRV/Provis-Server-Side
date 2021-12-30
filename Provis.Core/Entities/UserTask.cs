ac17fee5b0b3ee1c41857f4311d808218fb4d909
Pashuskui Volodumur <pashunskyi@gmail.com>
2021-12-30 19:12:20 +0200

    Add roles for tasks and update UserTask

---
+++ Provis.Core/Entities/UserTask.cs
@@ -0,0 +1,23 @@
+﻿using Provis.Core.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Provis.Core.Entities
+{
+    public class UserTask : IBaseEntity
+    {
+        public int TaskId { get; set; }
+
+        public Task Task { get; set; }
+
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+        [Required]
+        public int UserRoleTagId { get; set; }
+
+        [ForeignKey(nameof(UserRoleTagId))]
+        public UserRoleTag UserRoleTag { get; set; }
+    }
+}
