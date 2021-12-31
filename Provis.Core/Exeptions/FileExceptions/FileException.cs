0ca0bf0e08dc86e3522d52be4442b58abafc5c73
AndriyKovalets <andriykovalec@gmail.com>
2021-12-31 17:55:30 +0200

    Add serializable.

--- Provis.Core/Exeptions/FileExceptions/FileException.cs
+++ Provis.Core/Exeptions/FileExceptions/FileException.cs
@@ -1,15 +1,43 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Provis.Core.Exeptions.FileExceptions
 {
-    public class FileException: Exception
+    [Serializable]
+    public class FileException : Exception
     {
-        public override string Message { get; }
         public string Path { get; }
+
+        public FileException() { }
+
+        public FileException(string message)
+            : base(message) { }
+
+        public FileException(string message, Exception inner)
+            : base(message, inner) { }
+
         public FileException(string message, string path)
+            : base(message)
         {
-            Message = message;
             Path = path;
+        }
+
+        protected FileException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            this.Path = info.GetString("Path");
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException("info");
+            }
+
+            info.AddValue("Path", Path);
+
+            base.GetObjectData(info, context);
         }
     }
 }
