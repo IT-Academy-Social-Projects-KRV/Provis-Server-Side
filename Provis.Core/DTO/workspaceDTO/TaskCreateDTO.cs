d8be6877b223828f446d7440c4ea4ae6946dd3e9
Olexandr Sidletskyi <scasha2000@gmail.com>
2022-01-01 22:05:54 +0200

    add TaskCreateDTO

---
+++ Provis.Core/DTO/workspaceDTO/TaskCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Provis.Core.DTO.workspaceDTO
+{
+    public class TaskCreateDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfEnd { get; set; }
+        public int WorkspaceID { get; set; }
+        public int StatusId { get; set; }
+    }
+}
