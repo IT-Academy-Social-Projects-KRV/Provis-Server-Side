4c5a315986e53e0ff7ea8048bfaaa3c83290660b
Olexandr Sidletskyi <scasha2000@gmail.com>
2022-01-02 06:35:52 +0200

    add property assignedUser

--- Provis.Core/DTO/workspaceDTO/TaskCreateDTO.cs
+++ Provis.Core/DTO/workspaceDTO/TaskCreateDTO.cs
@@ -9,5 +9,12 @@ namespace Provis.Core.DTO.workspaceDTO
         public DateTime DateOfEnd { get; set; }
         public int WorkspaceID { get; set; }
         public int StatusId { get; set; }
+        public AssignedUserOnTask[] assignedUser { get; set; }
+    }
+    public class AssignedUserOnTask
+    {
+        public string UserId { get; set; }
+        public int TaskId { get; set; }
+        public int RoleTagId { get; set; }
     }
 }
