f346264c7b9b6ee84b0ba3beaf3d53f992561378
mayor.sheka <mayor.sheka@ukr.net>
2022-02-21 22:58:02 +0300

    update metrics

--- Provis.Core/Services/TaskService.cs
+++ Provis.Core/Services/TaskService.cs
@@ -560,6 +560,8 @@ namespace Provis.Core.Services
             var user = await _userWorkspaceRepository
                     .GetFirstBySpecAsync(userSpecification);
 
+            var currentUserRole = userTaskMember.UserRoleTagId;
+
             if (task.TaskCreatorId == userId ||
                 (WorkSpaceRoles)user.RoleId == WorkSpaceRoles.OwnerId)
             {
@@ -576,7 +578,7 @@ namespace Provis.Core.Services
                 }
 
                 _metrics.Measure.Counter.Decrement(WorkspaceMetrics.TaskRolesCountByWorkspace,
-                    MetricTagsConstructor.TaskRolesCountByWorkspace(changeRoleDTO.WorkspaceId, userTaskMember.UserRoleTagId));
+                    MetricTagsConstructor.TaskRolesCountByWorkspace(changeRoleDTO.WorkspaceId, currentUserRole));
 
                 _metrics.Measure.Counter.Increment(WorkspaceMetrics.TaskRolesCountByWorkspace,
                     MetricTagsConstructor.TaskRolesCountByWorkspace(changeRoleDTO.WorkspaceId, changeRoleDTO.RoleId));
